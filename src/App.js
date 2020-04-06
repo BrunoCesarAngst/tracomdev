b3331d96d53b119c61d219ef1c8fce52acf971ed
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-04-06 03:44:12 -0300

    rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
   return (
-    <div>
-      <p>Site do Bruno César Angst</p>
-    </div>
+    <Router history={history}>
+      <Routes />;
+    </Router>
   );
 }
 
