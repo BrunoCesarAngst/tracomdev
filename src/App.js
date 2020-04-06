9a127dd2c64b790ec029cd65b04e4d24e0bbc55b
brunocesarangst <bruno.angst@rede.ulbra.br>
2020-04-06 12:42:42 -0300

    styles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Router } from 'react-router-dom';
 
 import './config/reactotronConfig';
 
+import GlobalStyle from './styles/global';
+
 import Routes from './routes';
 import history from './services/history';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
